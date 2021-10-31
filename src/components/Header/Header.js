762e15df544035fb5783c041fe13394a5eb42e85
NadiaDipa <nadiaislamdipa1@gmail.com>
2021-10-31 23:44:37 +0600

    added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -55,51 +55,60 @@
 
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import logo from "./../../logo.png";
 
 const Header = () => {
      return (
         <div>
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow fs-6 fw-bold">
                 <div class="container-fluid">
-                    <a class="navbar-brand nav-bar fs-3" href="/home">Food <span className="text-primary">Delivery</span></a>
+                    <img width="80px" src={logo} alt="" />{" "}
+                    <a class="navbar-brand nav-bar fs-3" href="/home">Food <span className="text-danger">Delivery</span></a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-5">
                             <li class="nav-item">
-                                <NavLink className="link nav-link"
+                                <NavLink className = "link nav-link text-danger"
                                     to="/home"
                                 >
                                     Home
                                 </NavLink>
                             </li>
                             <li class="nav-item">
-                                <NavLink className="link nav-link"
+                                <NavLink className = "link nav-link text-danger"
                                     to="/manage-orders"
                                 >
                                     Manage Orders
                                 </NavLink>
                             </li>
                             <li class="nav-item">
-                                <NavLink className="link nav-link"
+                                <NavLink className = "link nav-link text-danger"
                                     to="/add-services"
                                 >
                                     Add Service
                                 </NavLink>
                             </li>
                             <li class="nav-item">
-                                <NavLink className="link nav-link"
+                                <NavLink className = "link nav-link text-danger"
                                     to="/my-orders"
                                 >
                                     My Orders
                                 </NavLink>
                             </li>
                             <li class="nav-item">
-                                <NavLink className="link nav-link"
+                                <NavLink className = "link nav-link text-danger"
                                     to="/services"
                                 >
                                     Services
+                                </NavLink>
+                            </li>
+                            <li class="nav-item">
+                                <NavLink className = "link nav-link text-danger"
+                                    to="/login"
+                                >
+                                    Login
                                 </NavLink>
                             </li>
                         </ul>
